a29957aa6e1988462a6058f8bf2fa4833d9c3799
manhtai <vo@manhtai.com>
2016-06-19 17:58:14 +0700

    Init numberpad

--- Caculator/ViewController.swift
+++ Caculator/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let currentTextInDisplay = display.text!
+            display.text = currentTextInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicSymbol = sender.currentTitle {
+            if mathematicSymbol == "π" {
+                 display.text = String(M_PI)
+            }
+        }
     }
-
-
 }
 
